30ba72bd025846a7e5cdcca2328363d6e3e68db0
Tim Gels <tgels@101solutions.nl>
2022-05-19 12:03:52 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-refresher-virtual-scroll-repro',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
